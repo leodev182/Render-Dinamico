459d06e7278fbd04150d49caf1087794162f0a8f
Leonardo Duque <leoartdev@gmail.com>
2023-11-05 16:53:16 -0300

    Se inicia el proyecto con 4 componentes iniciales

---
+++ src/components/listado/Listado.jsx
@@ -0,0 +1,34 @@
+import { Table } from "react-bootstrap";
+
+export const Listado = ({ colaboradores }) => {
+  //   console.table(colaboradores);
+
+  return (
+    <>
+      <Table responsive striped bordered>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Edad</th>
+            <th>Cargo</th>
+            <th>Telefono</th>
+          </tr>
+        </thead>
+        <tbody>
+          {colaboradores.map((colaborador) => (
+            <tr key={colaborador.id}>
+              <td>{colaborador.nombre}</td>
+              <td>{colaborador.correo}</td>
+              <td>{colaborador.edad}</td>
+              <td>{colaborador.cargo}</td>
+              <td>{colaborador.telefono}</td>
+            </tr>
+          ))}
+
+          {/* {renderTablaColaboradores()} */}
+        </tbody>
+      </Table>
+    </>
+  );
+};
